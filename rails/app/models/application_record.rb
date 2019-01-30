11c39fe72755ab1b59b0f18ee9c261cea1f3d026
Devin Starks <DevStarks@gmail.com>
2019-01-29 19:05:58 -0500

    rails model error logging

--- rails/app/models/application_record.rb
+++ rails/app/models/application_record.rb
@@ -1,3 +1,17 @@
 class ApplicationRecord < ActiveRecord::Base
+  after_validation :log_errors, :if => Proc.new { |m| m.errors }
+
   self.abstract_class = true
+
+  private
+
+  def log_errors
+    error = <<~HEREDOC
+      \n#{self.inspect}
+      Validation error(s):
+      #{self.errors.full_messages.join("\n")}
+    HEREDOC
+
+    Rails.logger.debug error.red
+  end
 end
