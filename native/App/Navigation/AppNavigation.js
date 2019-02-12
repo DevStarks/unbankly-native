53fd16b70c794be430d256a6c4294eab9a926ca5
Devin Starks <DevStarks@gmail.com>
2019-02-12 09:17:32 -0800

    redux devtools

--- native/App/Navigation/AppNavigation.js
+++ native/App/Navigation/AppNavigation.js
@@ -1,6 +1,7 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation'
 import LaunchScreen from '../Containers/LaunchScreen'
 import AuthScreen from '../Containers/AuthScreen'
+// import LoanScreen from '../Containers/LoanScreen'
 
 import styles from './Styles/NavigationStyles'
 
@@ -8,6 +9,7 @@ import styles from './Styles/NavigationStyles'
 const PrimaryNav = createStackNavigator({
   LaunchScreen: { screen: LaunchScreen },
   AuthScreen: { screen: AuthScreen }
+  // LoanScreen: { screen: LoanScreen }
 }, {
   // Default config for all screens
   headerMode: 'none',
